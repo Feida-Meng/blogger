9aecfde4f886cc59bd24f07c82bc7b3aa0a9e548
codonculous <feidameng01@gmail.com>
2017-07-22 23:05:27 -0400

    created input form for new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderField(field) {
+    return(
+      <div className='form-group'>
+        <label> {field.label} </label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+      </div>
+    );
+  }
 
   render() {
     return (
       <div>
-        
+        <form>
+          <Field
+            name='title'
+            label='Title'
+            component={this.renderField}
+          />
+          <Field
+            name='categories'
+            label='Post Categories'
+            component={this.renderField}
+          />
+
+          <Field
+            name='content'
+            label='Post content'
+            component={this.renderField}
+          />
+
+
+
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+function validate() {
+  
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm' //has to be unique, name of the form to distingush different forms on the same page
+})(PostsNew);
