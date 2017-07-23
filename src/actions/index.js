cc01aabb314638dd8258ba996f668b9bf9384121
codonculous <feidameng01@gmail.com>
2017-07-22 21:52:34 -0400

    Connected fetch_post action with component posts_index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
-const ROOT_URL = 'https://reduxblog.herokuapp.com/'
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api/'
 const K = '?key=alien1234';
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${K}`);
-
+  const request = axios.get(`${ROOT_URL}posts${K}`);
   return {
     type: FETCH_POSTS,
     payload: request
   };
 }
+
+// export function fetchPosts() {
+//   const request = axios.get(`${ROOT_URL}posts${K}`);
+//   return {
+//     type: FETCH_POSTS,
+//     payload: request
+//   };
+// }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,45 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-export default class PostsIndex extends Component {
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>
+          <h4>{post.title}</h4>
+          <p>Categories: {post.categories}</p>
+          <p>Post: {post.content}</p>  
+        </li>
+      );
+    });
+  }
+
   render() {
+
     return (
       <div>
-        Post Index
+        <h3> Post </h3>
+        <ul>
+          {this.renderPosts()}
+        </ul>
       </div>
     );
 
   }
 }
+
+function mapStationToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStationToProps, { fetchPosts })(PostsIndex);
+//{ fetchPosts } is same as the follow
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
