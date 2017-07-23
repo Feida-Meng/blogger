2350e98beb7d0b2284bee2e84d46f88c271fdd75
codonculous <feidameng01@gmail.com>
2017-07-23 01:15:16 -0400

    created fetch_post action creator and wired it to reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POSTS= 'create_posts';
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api/'
 const K = '?key=alien1234';
@@ -8,6 +9,14 @@ export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}posts${K}`);
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}posts/${id}${K}`);
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
